07a8e9c5568dc2485f740f34bc364d51c50a4186
Brandon Trautmann <btrautmann@users.noreply.github.com>
2021-10-30 21:26:41 -0400

    Fix import.

--- lib/features/customer_details/view/customer_details_page.dart
+++ lib/features/customer_details/view/customer_details_page.dart
@@ -3,7 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hydrawise/core-ui/widgets/h_stretch.dart';
 import 'package:hydrawise/features/customer_details/customer_details.dart';
-import 'package:hydrawise/features/login/cubit/login_cubit.dart';
+import 'package:hydrawise/features/login/login.dart';
 import 'package:hydrawise/features/weather/weather.dart';
 import 'package:intl/intl.dart';
 
