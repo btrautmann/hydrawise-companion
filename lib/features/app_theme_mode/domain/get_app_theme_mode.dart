626db6ae2c0638fb3bee9d474e09ae4def0ebedf
Brandon Trautmann <btrautmann@users.noreply.github.com>
2021-11-03 17:02:59 -0400

    get and set next poll time tests

--- lib/features/app_theme_mode/domain/get_app_theme_mode.dart
+++ lib/features/app_theme_mode/domain/get_app_theme_mode.dart
@@ -17,9 +17,7 @@ class GetAppThemeModeFromStorage implements GetAppThemeMode {
   @override
   Future<ThemeMode> call() async {
     final themeModeString = await _dataStorage.getString('app_theme_mode');
-    final mode = themeModeString == null
-        ? ThemeMode.system
-        : themeModeString.toThemeMode();
+    final mode = themeModeString == null ? ThemeMode.system : themeModeString.toThemeMode();
     return mode;
   }
 }
--- test/features/customer_details/domain/clear_customer_details_test.dart
+++ test/features/customer_details/domain/clear_customer_details_test.dart
@@ -5,15 +5,41 @@ import 'package:hydrawise/features/customer_details/customer_details.dart';
 void main() {
   group('ClearCustomerDetailsFromStorage', () {
     final storage = InMemoryStorage();
+    final repository = InMemoryCustomerDetailsRepository();
     final clearCustomerDetailsFromStorage = ClearCustomerDetailsFromStorage(
       dataStorage: storage,
-      customerDetailsRepository: InMemoryCustomerDetailsRepository(),
+      customerDetailsRepository: repository,
     );
     test('it sets api_key to an empty string', () async {
       await storage.setString('api_key', '1234');
       expect(await storage.getString('api_key'), '1234');
       await clearCustomerDetailsFromStorage();
       expect(await storage.getString('api_key'), '');
+    });
+
+    test('it clears all data in the repository', () async {
+      final zone = Zone(
+        id: 1,
+        physicalNumber: 1,
+        name: 'Fake Zone',
+        nextTimeOfWaterFriendly: '7:00',
+        secondsUntilNextRun: 60,
+        lengthOfNextRunTimeOrTimeRemaining: 500,
+      );
+      await repository.insertZone(zone);
+      final customer = CustomerIdentification(
+        activeControllerId: 1,
+        customerId: 1,
+        apiKey: 'fake-api-key',
+        lastStatusUpdate: 60,
+      );
+      await repository.insertCustomer(customer);
+
+      expect(await repository.getZones(), isNotEmpty);
+      expect(await repository.getCustomers(), isNotEmpty);
+      await clearCustomerDetailsFromStorage();
+      expect(await repository.getZones(), isEmpty);
+      expect(await repository.getCustomers(), isEmpty);
     });
   });
 }
