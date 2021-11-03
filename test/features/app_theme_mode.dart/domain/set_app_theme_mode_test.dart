e455a654d41936aba1440073cef0771137c796ee
Brandon Trautmann <btrautmann@users.noreply.github.com>
2021-11-03 16:39:38 -0400

    Get and set app theme mode tests

---
+++ test/features/app_theme_mode.dart/domain/set_app_theme_mode_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hydrawise/core/core.dart';
+import 'package:hydrawise/features/app_theme_mode/app_theme_mode.dart';
+
+void main() {
+  group('SetAppThemeModeInStorage', () {
+    final storage = InMemoryStorage();
+    final setAppThemeMode = SetAppThemeModeInStorage(storage);
+
+    test('it sets the app ThemeMode at app_theme_mode', () async {
+      await setAppThemeMode(ThemeMode.dark);
+      expect(await storage.getString('app_theme_mode'), 'ThemeMode.dark');
+    });
+  });
+}
