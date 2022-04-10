34fd38b2f521528ea32e69c6140db11868e59078
Brandon Trautmann <btrautmann@users.noreply.github.com>
2022-04-10 13:27:13 -0400

    get auth failures test

--- mobile/app/test/auth/get_auth_failures_test.dart
+++ mobile/app/test/auth/get_auth_failures_test.dart
@@ -1,5 +1,27 @@
+import 'dart:async';
+
 import 'package:flutter_test/flutter_test.dart';
+import 'package:irri/auth/auth.dart';
 
 void main() {
-  group('GetAuthFailures', () {});
+  group('GetAuthFailures', () {
+    late StreamController controller;
+
+    setUp(() {
+      controller = StreamController<void>();
+    });
+
+    tearDown(() {
+      controller.close();
+    });
+
+    test('it returns the stream from the provided streamcontroller', () async {
+      final subject = GetAuthFailures(
+        authFailuresController: controller,
+      );
+      final stream = await subject.call();
+      controller.add(null);
+      await expectLater(stream, emits(null));
+    });
+  });
 }
