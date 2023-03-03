d2afb00202a32db1f6e51a160d5c58ad4535a97b
Brandon Trautmann <btrautmann@users.noreply.github.com>
2023-03-02 23:53:49 -0500

    use env variable for tasks api endpoint

--- mobile/api_wrapper/bin/test_tasks.dart
+++ mobile/api_wrapper/bin/test_tasks.dart
@@ -1,12 +1,17 @@
+import 'package:dotenv/dotenv.dart';
 import 'package:http/http.dart' as http;
 import 'package:shelf/shelf.dart';
 
 /// Dummy end-point for calling ruby's create function
 /// to create a task
 class TestTasks {
+  TestTasks(this.env);
+
+  final DotEnv env;
+  
   Future<Response> call(Request request) async {
     final response = await http.post(
-      Uri.https('tasks-5rvb357uza-ue.a.run.app', 'create'),
+      Uri.https(env['TASKS_API_END_POINT']!, 'create'),
     );
     print(response.body);
     return Response.ok(response.body);
