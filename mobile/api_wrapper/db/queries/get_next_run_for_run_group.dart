db3662386fd9a69d5723bc9476e53623efabd501
Brandon Trautmann <btrautmann@users.noreply.github.com>
2023-03-14 17:19:38 -0400

    try adding offset

--- mobile/api_wrapper/db/queries/get_next_run_for_run_group.dart
+++ mobile/api_wrapper/db/queries/get_next_run_for_run_group.dart
@@ -20,33 +20,31 @@ class GetNextRunForRunGroup {
     // time rather than lastRunTime if suspended.
     // If the group has never run, consider last run time as epoch
     final groupLastRunTime = group.lastRunTime ?? epochUtc();
-    print('Group lastRunTime is $groupLastRunTime');
     final programFrequency = program.frequency;
-    print('Program frequency is $programFrequency');
     final controller = await _getControllerById(program.controllerId);
     final timezone = controller.timezone;
-    print('Controller ${controller.id} is in timezone $timezone');
     final location = tz.getLocation(timezone);
-    // Seed time is the current time adjusted to adopt the hour
-    // and minute of the group's intended run time
+    // Get user local time to figure out next run time, then convert
+    // that to UTC and return
     final localTime = tz.TZDateTime.from(
-      nowUtc().copyWith(
-        hour: group.startHour,
-        minute: group.startMinute,
-        second: 0,
-        millisecond: 0,
-        microsecond: 0,
-      ),
+      nowUtc()
+          .copyWith(
+            hour: group.startHour,
+            minute: group.startMinute,
+            second: 0,
+            millisecond: 0,
+            microsecond: 0,
+          )
+          .add(
+            Duration(milliseconds: location.currentTimeZone.offset),
+          ),
       location,
     );
-    print(
-      'localTime is $localTime in timezone ${localTime.timeZoneName} with offset ${localTime.timeZoneOffset}',
-    );
-    print('localTime isUtc? ${localTime.isUtc}');
+    print('localTime is $localTime with timezone ${localTime.timeZoneName}');
+
     final eligibleNextRunDays = List.generate(8, (index) {
       return localTime.add(Duration(days: index));
     });
-    print('Eligible days for next run are $eligibleNextRunDays');
     const oneDay = Duration(days: 1);
     final nextRun = eligibleNextRunDays.firstWhere((day) {
       // Run groups run at most once a day, so next run must be
